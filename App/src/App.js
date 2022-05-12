1c898889bc65537f51b5753e95f9ccddec847122
prince-hope1975 <amachreeprince7@gmail.com>
2022-05-12 19:06:21 +0100

    Added wallet connect

--- App/src/App.js
+++ App/src/App.js
@@ -4,20 +4,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import ToolBar from "./components/ToolBar";
+import { AppProvider } from "./context";
 
 function App() {
   return (
     <>
-    <ToolBar>
-
-    </ToolBar>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="register" element={<Register />} />
-          {/* <Route path="profile" element={<Portfolio />} /> */}
-        </Routes>
-      </BrowserRouter>
+      <AppProvider>
+        <ToolBar></ToolBar>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="register" element={<Register />} />
+            {/* <Route path="profile" element={<Portfolio />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </AppProvider>
     </>
   );
 }
--- App/src/pages/Home.js
+++ App/src/pages/Home.js
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import ButtonComp from "@mui/material/Button";
-import {ALGO_WalletConnect as MyAlgoConnect, loadStdlib}from "@reach-sh/stdlib"
-const reach = loadStdlib(process.env.REACH_CONNECTOR_MODE="ETH")
+import { loadStdlib } from "@reach-sh/stdlib";
+import { ALGO_MyAlgoConnect as MyAlgoConnect } from "@reach-sh/stdlib";
+import { useGlobalContext } from "../context";
 
-
+const reach = loadStdlib((process.env.REACH_CONNECTOR_MODE = "ALGO"));
+reach.setWalletFallback(
+  reach.walletFallback({
+    providerEnv: "MainNet",
+    MyAlgoConnect,
+  })
+);
 const Home = () => {
-  const [account, setAccount] = useState(null)
-  const [loading, setLoading] = useState(false)
-  useEffect(()=>{
-   reach.getDefaultAccount()
-  },[loading])
+  const { dispatch , state} = useGlobalContext();
+  // const [account, setAccount] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    console.log(state)
+  }, [state.networkAccount]);
 
-  const connectAcct = async ()=>{
-    const acct = await reach.getDefaultAccount()
-    setAccount(account)
-  }
+  const connectAcct = async () => {
+    try {
+      const newAccount = await reach.getDefaultAccount();
+      dispatch({ type: "SET_ACCOUNT", payload: newAccount });
+      // reach.connectAccount(newAccount);
+      console.log(state.networkAccount)
+    } catch (error) {
+      setLoading(!loading);
+      console.log(error);
+    }
+  };
   return (
     <Container>
-      <Button >
-        Register
-      </Button>  
-      <Button >
-        connect
-      </Button>
+      <Button>Register</Button>
+      <Button onClick={connectAcct}>connect</Button>
     </Container>
   );
 };
@@ -32,7 +43,7 @@ const Container = styled.div`
   padding-top: 4rem;
 `;
 const Button = styled(ButtonComp)`
-  background-color: hsl(300, 100%, 25%, 0.3)  !important;
+  background-color: hsl(300, 100%, 25%, 0.3) !important;
   color: white !important;
   box-shadow: 1px 1px 1px #333 !important;
 `;
